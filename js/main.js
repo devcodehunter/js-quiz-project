942ccaa0bd2c498aee9abc5cd24518320ac1fda0
Gulam Mahabub Morshed <shawonahmed78@gmail.com>
2017-11-04 21:39:29 +0600

    Quiz using javascript

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+function getResult() {
+
+	var totalQues=5;
+	var score = 0;
+
+	var q1 = document.forms['quiz']['q1'].value,
+	q2 = document.forms['quiz']['q2'].value,
+	q3 = document.forms['quiz']['q3'].value,
+	q4 = document.forms['quiz']['q4'].value,
+	q5 = document.forms['quiz']['q5'].value;
+
+	// Validation
+	for(var i=1; i<=totalQues; i++){
+		if(eval('q'+i) === null || eval('q'+i) === ''){
+		alert('You missed question '+i);
+		return false;
+		}
+	}
+
+	// Set Correct Answer
+
+	var answer= ["b","a","d","b","d"];
+
+	for(var i=1; i <= totalQues; i++){
+		if(eval('q'+i) === answer[i-1]){
+			score++;
+		}
+	}
+
+
+
+	//Display Data
+	var result="<p>You have scored "+score+" out of "+totalQues+"</p>";
+	document.getElementById("demo").innerHTML = result;
+
+	return false;
+}
+
---
+++ quiz.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ligin Panel</title>
+	<link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+	
+	<div class="container">
+		<header class="quiz_title">
+			<h1 class="quiz_title--headline">Simple JavaScript Quiz</h1>
+			<p>Test your knowledge in <strong>JavaScript Fundamental</strong></p>
+
+		</header>
+		<section>
+			<div id="demo"></div>
+
+			<form name="quiz" onsubmit="return getResult()">
+				<div class="quiz-head">
+					<div>
+						<h2>1. In which HTML element do we put in JavaScript code ?</h2>
+						<div class="quiz-ques">
+							
+							<input type="radio" name="q1" value="a" id="q1a"> a. &lt;js&gt; </br>
+							<input type="radio" name="q1" value="b" id="q1b"> b. &lt;script&gt;</br>
+							<input type="radio" name="q1" value="c" id="q1c"> c. &lt;body&gt;</br>		
+							<input type="radio" name="q1" value="d" id="q1d"> d. &lt;head&gt;	
+						</div>
+					</div>
+					<div>
+						<h2>2. In which HTML element do we put in JavaScript code ?</h2>
+						<div class="quiz-ques">
+							
+							<input type="radio" name="q2" value="a" id="q2a"> a. &lt;js&gt; </br>
+							<input type="radio" name="q2" value="b" id="q2b"> b. &lt;script&gt;</br>
+							<input type="radio" name="q2" value="c" id="q2c"> c. &lt;body&gt;</br>		
+							<input type="radio" name="q2" value="d" id="q2d"> d. &lt;head&gt;	
+						</div>
+					</div>
+					<div>
+						<h2>3. In which HTML element do we put in JavaScript code ?</h2>
+						<div class="quiz-ques">
+							
+							<input type="radio" name="q3" value="a" id="q3a"> a. &lt;js&gt; </br>
+							<input type="radio" name="q3" value="b" id="q3b"> b. &lt;script&gt;</br>
+							<input type="radio" name="q3" value="c" id="q3c"> c. &lt;body&gt;</br>		
+							<input type="radio" name="q3" value="d" id="q3d"> d. &lt;head&gt;	
+						</div>
+					</div>
+					<div>
+						<h2>4. In which HTML element do we put in JavaScript code ?</h2>
+						<div class="quiz-ques">
+							
+							<input type="radio" name="q4" value="a" id="q4a"> a. &lt;js&gt; </br>
+							<input type="radio" name="q4" value="b" id="q4b"> b. &lt;script&gt;</br>
+							<input type="radio" name="q4" value="c" id="q4c"> c. &lt;body&gt;</br>		
+							<input type="radio" name="q4" value="d" id="q4d"> d. &lt;head&gt;	
+						</div>
+					</div>
+					<div>
+						<h2>5. In which HTML element do we put in JavaScript code ?</h2>
+						<div class="quiz-ques">
+							
+							<input type="radio" name="q5" value="a" id="q5a"> a. &lt;js&gt; </br>
+							<input type="radio" name="q5" value="b" id="q5b"> b. &lt;script&gt;</br>
+							<input type="radio" name="q5" value="c" id="q5c"> c. &lt;body&gt;</br>		
+							<input type="radio" name="q5" value="d" id="q5d"> d. &lt;head&gt;	
+						</div>
+					</div>
+					
+					
+					<input type="submit" value="Submit Anwser">
+				</div>
+				 
+			</form>
+		</section>
+
+		<footer>
+			<p>Copywrite &copy; All Rights Reserved</p>
+		</footer>
+	</div>
+
+	
+ 
+	<script src="js/main.js" type="text/javascript"></script>
+	
+</body>
+</html>
